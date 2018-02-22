5874875cedd0734d841b0c6ea7378b8dff00e74c
jkarlin929 <osida11572@gmail.com>
2018-02-22 14:43:34 -0500

    added map and bike path layer

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
+const bikesRouter = require('./routes/bike-routes');
+
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello Friend');
