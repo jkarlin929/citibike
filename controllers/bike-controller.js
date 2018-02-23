b68b32ff472e58a51d98f49d234a6a8cb93e7c8d
jkarlin929 <osida11572@gmail.com>
2018-02-23 15:21:21 -0500

    added markers for all citibike positions

--- controllers/bike-controller.js
+++ controllers/bike-controller.js
@@ -1,5 +1,19 @@
-module.exports = {
-  index: (req, res) => {
-    res.render('bikes/index')
-  }
-};
+const axios = require('axios');
+const bikeController = {};
+
+bikeController.index = (req,res) => {
+axios({
+  method: 'get',
+  url: 'https://feeds.citibikenyc.com/stations/stations.json'
+}).then(data => {
+  // console.log(data.data.stationBeanList)
+  res.render('bikes/index', {
+    bikes: data.data.stationBeanList
+  })
+}).catch(err => {
+  console.log(err)
+  })
+}
+
+
+module.exports = bikeController
