e2a2ccd4f702761a806014d4d40a0bf026dc9849
jkarlin929 <osida11572@gmail.com>
2018-02-27 14:39:27 -0500

    add directions function to map

--- controllers/bike-controller.js
+++ controllers/bike-controller.js
@@ -19,19 +19,6 @@ axios({
   });
 };
 
-// bikeController.index = (req,res) => {
-// axios({
-//   method: 'get',
-//   url: 'https://feeds.citibikenyc.com/stations/stations.json'
-// }).then(data => {
-//   res.render('bikes/index', {
-//     bikes: data.data.stationBeanList
-//   })
-// }).catch(err => {
-//   res.status(400).json(err);
-//   });
-// };
-
 bikeController.show = (req, res) => {
   Bike.findById(req.params.id)
   .then(bike => {
